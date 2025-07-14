f7eb52e019048cdb8b98176efbc5f743359a6e66
mutheeEverlyn <muthee.14952@students.kyu.ac.ke>
2025-07-14 13:40:33 +0300

    changes

--- client/src/config.js
+++ client/src/config.js
@@ -1,1 +1,1 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api"; 
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://week-5-web-sockets-assignment-n1wd.onrender.com/api"; 

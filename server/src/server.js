5cad952878f3daed130245538b8e0cb976059a8d
mutheeEverlyn <muthee.14952@students.kyu.ac.ke>
2025-07-14 11:56:19 +0300

    port changes

--- server/src/server.js
+++ server/src/server.js
@@ -46,7 +46,7 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("Connected to MongoDB");
-    const PORT = process.env.PORT || 5001;
+    const PORT = process.env.PORT || 5000;
     httpServer.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
